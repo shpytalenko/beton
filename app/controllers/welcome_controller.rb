63e4a22ba46e18e8c91a6d87cf5c13f45a9f3d31
Pasha <shpytalenko@gmail.com>
2014-01-05 22:37:53 +0200

    init commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def main
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beton::Application.config.secret_token = '12d46efcbdefd35f2ac97a69de368cfa7d7ccbdd581cc067711542ca9debebf9e22584ab70dade782899f2fe08820fcb30bba60a179b059ce7a428b5a3205332'
